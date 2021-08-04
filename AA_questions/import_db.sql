20102cfdc696ae5e19256f5763b8cfe6b5aa2bc7
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-08-04 11:48:01 -0700

    complete table question _follow

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
     question_id INTEGER NOT NULL
@@ -11,11 +11,15 @@ CREATE TABLE questions (
     title TEXT NOT NULL,
     body TEXT,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE question_follows (
+    qf_id INTEGER PRIMARY KEY,
     q_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-)
+
+    FOREIGN KEY (q_id) REFERENCES questions(q_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
