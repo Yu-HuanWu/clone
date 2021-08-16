db0d31f48fae21ce85117756dcd3d691776acdea
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-08-16 15:01:05 -0700

    added sessions and users controllers

--- W7D1/99cats/app/models/user.rb
+++ W7D1/99cats/app/models/user.rb
@@ -4,4 +4,36 @@ class User < ApplicationRecord
     validates :session_token, presence: true, uniqueness: true
     validates :password, length: { minimum: 6 }, allow_nil: true
     after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(user_name, pass_word)
+        user = User.find_by(user_name: user_name)
+        if user && user.is_password?(pass_word)
+            user
+        else
+            nil
+        end
+    end
+    
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(pass_word)
+        @password = pass_word
+        self.password_digest = BCrypt::Password.create(pass_word)
+    end
+
+    def is_password?(pass_word)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(pass_word)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
 end
