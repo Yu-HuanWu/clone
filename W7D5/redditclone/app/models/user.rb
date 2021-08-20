dfa03c2baac68da155692177f30f8b1f0b4a313a
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-08-20 11:03:13 -0700

    Finished Sub and Post models, moving on to Sub and Post controllers

--- W7D5/redditclone/app/models/user.rb
+++ W7D5/redditclone/app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
@@ -8,6 +19,14 @@ class User < ApplicationRecord
     #SPIRE
     attr_reader :password
 
+    has_many :subs,
+        foreign_key: :moderator_id,
+        class_name: :Sub
+
+    has_many :posts,
+        foreign_key: :author_id,
+        class_name: :Post
+
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.is_password?(password)
@@ -17,7 +36,7 @@ class User < ApplicationRecord
         end
     end
 
-    def password =(password)
+    def password=(password)
         self.password_digest = BCrypt::Password.create(password)
         @password = password
     end
