eede8c20d3ee2a5e8fdd691a5f341675844f4c58
jayzizzle <dj.jonzamora@gmail.com>
2021-09-14 15:24:59 -0700

    solved pathing issue

--- W11D2/Todos/todo_redux.jsx
+++ W11D2/Todos/todo_redux.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import todosReducer from './frontend/reducers/todos_reducer'
-
+import configureStore from './frontend/store/store';
+import todosReducer from './frontend/reducers/todos_reducer';
+import { receiveTodo, receiveTodos } from './frontend/actions/todo_actions';
 function Root(){
     return (
         <h1>Todos App</h1>
@@ -10,6 +11,11 @@ function Root(){
 
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main");
-    // window.todosReducer= todosReducer;
+    window.todosReducer = todosReducer;
+    window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
+    // window.store = configureStore();
+    const store = configureStore();
+    window.store = store;
     ReactDOM.render(<Root/>, main);
 });
